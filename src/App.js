54b97986336661644f85dc05cea07d505cc3ca1d
sowmiyanarayani <sowmiya2692001@gmail.com>
2024-04-11 20:34:34 +0530

    firstCommit3

--- src/App.js
+++ src/App.js
@@ -3,7 +3,25 @@ import './App.scss';
 
 const App = () =>
 	<div className="App">
-		Ready to start.
+		<a href="https://www.google.com">
+			<button style={ {
+				backgroundColor: 'red', color: 'black',
+				padding: '10px',
+			} }
+			>
+				button1
+			</button>
+
+		</a>
+		<a href="https://www.instagram.com">
+			<button style={ {
+				backgroundColor: 'blue', color: 'black',
+				padding: '10px',
+			} }
+			>
+				button2</button>
+
+		</a>
 	</div>;
 
 export default App;
